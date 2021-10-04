a0bb13c60c213623e465ebbb4777efb5e32982b3
Kirill Romanov <djaler1@gmail.com>
2021-10-04 12:16:34 +0300

    feat: initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <Child :[propName]="propValue"/>
+</template>
+
+<script lang="ts">
+    import { defineComponent, ref } from '@vue/composition-api';
+    import Child from '@/Child.vue';
+
+    export default defineComponent({
+        name: 'App',
+        components: { Child },
+        setup() {
+            const propName = 'foo';
+            const propValue = 'value';
+
+            return {
+                propName,
+                propValue,
+            };
+        },
+    });
+</script>
---
+++ src/Child.vue
@@ -0,0 +1,21 @@
+<template>
+    {{ foo }}/{{ bar }}
+</template>
+
+<script lang="ts">
+    import { defineComponent } from '@vue/composition-api';
+
+    export default defineComponent({
+        name: 'Child',
+        props: {
+            foo: {
+                type: String,
+                default: null,
+            },
+            bar: {
+                type: String,
+                default: null,
+            },
+        },
+    });
+</script>
